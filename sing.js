1982f7ea21b5d146ef89ad076977e74431ce5e94
SeungHwanOH <pro5kinghwan@gmail.com>
2021-11-23 15:02:53 +0900

    js

--- sing.js
+++ sing.js
@@ -1,3 +1,6 @@
-let button = () => {
-    alert('login Button !')
+function login() {
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("pw_filed").value;
+
+    window.alert(userEmail + " " + userPass);
 }
